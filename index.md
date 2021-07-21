785a60125495debca98eccfb40df28e939e71c56
Mattia Ficarelli <mattia.ficarelli@nhsx.nhs.uk>
2021-07-21 23:50:34 +0200

    Update

--- index.md
+++ index.md
@@ -1,17 +1,41 @@
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 
-# Open Health Statistics: The Cost of Antibiotics
+# Open Source Healthcare Statistics
+
+### Collecting and visualizing statistics on the cost of antibiotics 
 
 {% include update.html %}
 
 <hr class="nhsuk-u-margin-top-0 nhsuk-u-margin-bottom-6">
 
-Developed using a template for end-to-end open source analytics: python, [plotly](https://plotly.com/python/), [github.io](https://pages.github.com/), and [github actions](https://github.com/features/actions).
-
-## Total cost (£) of the antibiotics Amoxicillin, Doxycycline Hyclate, and Cefalexin per month 
+## Cost (£) of Amoxicillin, Doxycycline Hyclate, and Cefalexin per month 
 
 {% include plotly_obj.html %}
 
-## Total (£) of Amoxicillin, Doxycycline Hyclate, and Cefalexin per 1000 GP registered patients for the current year
+<div class="nhsuk-action-link">
+  <a class="nhsuk-action-link__link" href="assets/data/cost_antibiotics_per_month.csv">
+    <svg class="nhsuk-icon nhsuk-icon__arrow-right-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M12 2a10 10 0 0 0-9.95 9h11.64L9.74 7.05a1 1 0 0 1 1.41-1.41l5.66 5.65a1 1 0 0 1 0 1.42l-5.66 5.65a1 1 0 0 1-1.41 0 1 1 0 0 1 0-1.41L13.69 13H2.05A10 10 0 1 0 12 2z"></path>
+    </svg>
+    <span class="nhsuk-action-link__text">Download this dataset (.csv)</span>
+  </a>
+</div>
 
-<hr class="nhsuk-u-margin-top-0 nhsuk-u-margin-bottom-6">
+## Cost (£) of Amoxicillin, Doxycycline Hyclate, and Cefalexin per 1000 GP registered population for the current year
+
+## About this page
+
+This page is built using end-to-end open source analytical tools including: [The NHS Digital Service Manual](https://service-manual.nhs.uk/), [python](https://nhs-pycom.net/), [plotly](https://plotly.com/python/), [folium](http://python-visualization.github.io/folium/), (https://pages.github.com/), and [github actions](https://github.com/features/actions).
+
+<div class="nhsuk-action-link">
+  <a class="nhsuk-action-link__link" href="/open-health-statistics/blog">
+    <svg class="nhsuk-icon nhsuk-icon__arrow-right-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M12 2a10 10 0 0 0-9.95 9h11.64L9.74 7.05a1 1 0 0 1 1.41-1.41l5.66 5.65a1 1 0 0 1 0 1.42l-5.66 5.65a1 1 0 0 1-1.41 0 1 1 0 0 1 0-1.41L13.69 13H2.05A10 10 0 1 0 12 2z"></path>
+    </svg>
+    <span class="nhsuk-action-link__text">Find out how to build your own open analytics pipeline</span>
+  </a>
+</div>
+
+If you have any suggestions or questions, email: <a href="mailto:mattia.ficarelli@nhsx.nhs.uk">mattia.ficarelli@nhsx.nhs.uk</a>
--- run.py
+++ run.py
@@ -1,17 +1,18 @@
-
-import pandas as pd
-import requests
-from datetime import datetime
 import urllib.request
 from urllib.request import urlopen
-import json
+import requests
 import folium
+import json
+import copy
+import pandas as pd
 import plotly
 import plotly.express as px
+from datetime import datetime
 
-# Generate Data
 
-#API Query Start
+#Plot 1 start
+##API Query Start
+###Amoxicillin(0501013B0) cost data
 url_0501013B0 = "https://openprescribing.net/api/1.0/spending_by_ccg/?code=0501013B0&format=json"
 response_0501013B0 = urllib.request.urlopen(url_0501013B0)
 data_0501013B0 = json.loads(response_0501013B0.read())
@@ -20,6 +21,7 @@ flat_data_0501013B0 = flat_data_0501013B0.groupby(['row_name', 'row_id', 'date']
 flat_data_0501013B0 = flat_data_0501013B0.drop(columns=['items', 'quantity'])
 flat_data_0501013B0 .rename(columns={'actual_cost': 'Amoxicillin'}, inplace=True)
 
+###Doxycycline Hyclate(0501030I0) cost data
 url_0501030I0 = "https://openprescribing.net/api/1.0/spending_by_ccg/?code=0501030I0&format=json"
 response_0501030I0 = urllib.request.urlopen(url_0501030I0)
 data_0501030I0 = json.loads(response_0501030I0.read())
@@ -27,8 +29,8 @@ flat_data_0501030I0 = pd.json_normalize(data_0501030I0)
 flat_data_0501030I0 = flat_data_0501030I0.groupby(['row_name', 'row_id', 'date']).sum()
 flat_data_0501030I0 = flat_data_0501030I0.drop(columns=['items', 'quantity'])
 flat_data_0501030I0.rename(columns={'actual_cost': 'Doxycycline Hyclate'}, inplace=True)
-flat_data_0501030I0
 
+###Cefalexin(0501021L0) cost data
 url_0501021L0 = "https://openprescribing.net/api/1.0/spending_by_ccg/?code=0501021L0&format=json"
 response_0501021L0 = urllib.request.urlopen(url_0501021L0)
 data_0501021L0 = json.loads(response_0501021L0.read())
@@ -37,9 +39,9 @@ flat_data_0501021L0 = flat_data_0501021L0.groupby(['row_name', 'row_id', 'date']
 flat_data_0501021L0 = flat_data_0501021L0.drop(columns=['items', 'quantity'])
 flat_data_0501021L0.rename(columns={'actual_cost': 'Cefalexin'}, inplace=True)
 flat_data_0501021L0
-#API Query End
+##API Query End
 
-#Processing Plot 1
+##Data processing for plot 1
 join_1 = flat_data_0501013B0.join(flat_data_0501030I0, lsuffix='row_id', rsuffix='row_id')
 all_antibiotics_merged = join_1.join(flat_data_0501021L0, lsuffix='row_id', rsuffix='row_id')
 all_antibiotics_merged.fillna(0, inplace=True)
@@ -48,75 +50,46 @@ all_antibiotics = all_antibiotics_merged.reset_index()
 all_antibiotics.rename(columns={'row_name': 'Clinical Commissioning Group (CCG)', 'row_id': 'CCG code', 'date': 'Date'}, inplace=True)
 all_antibiotics_plot = all_antibiotics.groupby(['Date']).sum()
 all_antibiotics_plot = all_antibiotics_plot.reset_index()
-all_antibiotics_plot = all_antibiotics_plot.round(1)
-#end
+all_antibiotics_plot = all_antibiotics_plot.round(2)
+##Data processing end
 
-#Plot 1
+##Save data for plot 1 to csv
+fig_1_data = all_antibiotics_plot.copy()
+fig_1_data.rename(columns={
+"Amoxicillin": "Cost (£) of Amozicillin",
+"Doxycycline Hyclate": "Cost (£) of Doxycycline Hyclate", 
+"Cefalexin": "Cost (£) of Cefalexin",
+"Total cost of Amoxicillin, Doxycycline Hyclate, Cefalexin (£)": "Cost (£) of Amoxicillin, Doxycycline Hyclate, and Cefalexin"}, 
+inplace=True)
+fig_1_data.index.name = 'Unique ID'
+fig_1_data.to_csv("assets/data/cost_antibiotics_per_month.csv", index=False)
+##Save data end
+
+##Visualization Plot 1
 pd.options.plotting.backend = "plotly"
 fig = px.bar(all_antibiotics_plot, x='Date', y= ["Amoxicillin", "Doxycycline Hyclate", 'Cefalexin'],
 color_discrete_sequence=["#003087", "#0072CE", "#41B6E6"],
-labels={"value": "Cost (£)", "variable": "Antibiotic"})
+labels={"value": "Cost (£)", "variable": "Antibiotic"},  
+title= "Cost (£) of Amoxicillin, Doxycycline Hyclate, and Cefalexin per month")
 fig.update_layout(
     {"plot_bgcolor": "rgba(0, 0, 0, 0)", "paper_bgcolor": "rgba(0, 0, 0, 0)"},
     autosize=True,
     margin=dict(l=50, r=50, b=50, t=50, pad=4, autoexpand=True),
 )
-#End
+##Visualization Plot 1 end
 
-#CCG pop from NHS digital 
-csv_url = "https://files.digital.nhs.uk/40/2232E5/gp-reg-pat-prac-all.csv"
-req = requests.get(csv_url)
-url_content = req.content
-csv_file = open('ccg_pop.csv', 'wb')
-csv_file.write(url_content)
-csv_file.close()
-df1 = pd.read_csv('ccg_pop.csv')
-CCG_pop = df1.groupby(['CCG_CODE']).sum().reset_index()
-CCG_pop.rename(columns={'CCG_CODE': 'CCG code', 'NUMBER_OF_PATIENTS': 'Number of patients registered at GP practices'}, inplace=True)
-#end
-
-#Processing Plot 2 
-current_year = datetime.now().year
-current_year_str = str(current_year)
-all_antibiotics["Date"] = pd.to_datetime(all_antibiotics["Date"]).apply(lambda x: x.strftime("%Y"))
-all_antibiotics_current_year = all_antibiotics.loc[all_antibiotics['Date'] == current_year_str]
-df1 = all_antibiotics_current_year.groupby(["CCG code", "Clinical Commissioning Group (CCG)"]).sum()
-df2 = df1.drop(columns=['Amoxicillin', 'Doxycycline Hyclate', 'Cefalexin'])
-df3 = df2.reset_index()
-df4 = df3.join(CCG_pop, rsuffix='CCG code')
-df5 = df4.drop(columns=['CCG codeCCG code'])
-df5["Cost (£) of Amoxicillin, Doxycycline Hyclate, and Cefalexin per 1000 GP registered patients"] = df5["Total cost of Amoxicillin, Doxycycline Hyclate, Cefalexin (£)"]/(df5["Number of patients registered at GP practices"]/1000)
-df5.round(2)
-df6 = df5.sort_values(by= "Cost (£) of Amoxicillin, Doxycycline Hyclate, and Cefalexin per 1000 GP registered patients" , ascending=True)
-#end
-
-#Plot 2 
-with urlopen('https://openprescribing.net/api/1.0/org_location/?org_type=ccg') as response:
-    data_ccg_geojson = json.load(response)
-fig_2 = folium.Map(
-    location=[53, 0.13],
-    tiles="cartodbpositron",
-    zoom_start=6.3,
-)
-folium.Choropleth(
-    geo_data=data_ccg_geojson,
-    name="choropleth",
-    data=df6,
-    columns=["CCG code", "Cost (£) of Amoxicillin, Doxycycline Hyclate, and Cefalexin per 1000 GP registered patients"],
-    key_on="feature.properties.code",
-    fill_color="YlGnBu",
-    fill_opacity=0.9,
-    line_opacity=0.5,
-    legend_name="Cost (£) per 1000 GP registered patients",
-).add_to(fig_2)
-
-# fig_1 Write out to file (.html)
+##Write out to file (.html) Plot 1
 config = {"displayModeBar": False, "displaylogo": False}
 plotly_obj = plotly.offline.plot(
     fig, include_plotlyjs=False, output_type="div", config=config
 )
 with open("_includes/plotly_obj.html", "w") as file:
     file.write(plotly_obj)
+##Write out to file (.html) Plot 1 end
+#Plot 1 end
+
+#Plot 2 start
+#Plot 2 end
 
 # Grab timestamp
 data_updated = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
